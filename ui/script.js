50b2a726af287b794509b3b1cc3a1d2f80efe365
Matt Condon <matt.condon@mongodb.com>
2025-01-07 23:40:47 +0000

    ui: add navigation by gameweek (#15)

--- ui/script.js
+++ ui/script.js
@@ -1,31 +1,43 @@
+let gameweekIndex = 0;
+
 // Fetch the aggregated league table data from the JSON file
 fetch(filePath)
     .then(response => response.json())
     .then(data => {
-        const tableBody = document.getElementById('leagueTable').getElementsByTagName('tbody')[0];
-
-        data.Standings.forEach(entry => {
-            const row = document.createElement('tr');
-
-            // Add table cells
-            row.innerHTML = `
-                <td>${entry.Rank}</td>
-                <td>
-                    <a href="${entry.TeamUrl}">${entry.EntryName}</a>
-                    <div>${entry.PlayerName}</div>
-                </td>
-                <td>${entry.EventTotal}</td>
-                <td>${entry.StageTwoTotal}</td>
-                <td>${entry.Total}</td>
-            `;
-
-            tableBody.appendChild(row);
-        });
-
-        const eventElement = document.getElementById('event');
-        eventElement.textContent = `[${data.Event}]`;
+        setTableFromJsonData(data);
     })
-    .catch(error => console.error('Error fetching league table data:', error));
+    .catch(error => 
+        console.error('Error fetching league table data:', error)
+    );
+
+ 
+function setTableFromJsonData(data) {
+    const tableBody = document.getElementById('leagueTable').getElementsByTagName('tbody')[0];
+    // resets the table
+    tableBody.innerHTML = '';
+
+    gameweekIndex = data.Event;
+    data.Standings.forEach(entry => {
+        const row = document.createElement('tr');
+
+        // Add table cells
+        row.innerHTML = `
+                    <td>${entry.Rank}</td>
+                    <td>
+                        <a href="${entry.TeamUrl}">${entry.EntryName}</a>
+                        <div>${entry.PlayerName}</div>
+                    </td>
+                    <td>${entry.EventTotal}</td>
+                    <td>${entry.StageTwoTotal}</td>
+                    <td>${entry.Total}</td>
+                `;
+
+        tableBody.appendChild(row);
+    });
+
+    const eventElement = document.getElementById('event');
+    eventElement.textContent = `Gameweek ${gameweekIndex}`;
+}
 
 function sortTable(columnIndex) {
     const table = document.getElementById("leagueTable");
@@ -67,4 +79,19 @@ function sortTable(columnIndex) {
             header.classList.add(newDirection === "asc" ? "sorted-asc" : "sorted-desc");
         }
     });
+}
+
+function navigate(navDirection) {
+    gameweekIndex += navDirection;
+    filePath = `../data/view/aggregated-league-table-${gameweekIndex}.json`;
+
+    fetch(filePath)
+        .then(response => response.json())
+        .then(data => {
+            setTableFromJsonData(data);
+        })
+        .catch(error => {
+            console.error('Error fetching league table data:', error);
+            gameweekIndex -= navDirection;
+        });
 }
